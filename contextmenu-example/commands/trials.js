15ca6bccbfcf7a5f0f580c5ebb4fa5730a14f4e1
Milim <25005864+Electrocute4u@users.noreply.github.com>
2025-02-11 16:04:32 +0100

    Remove a unused dir

--- contextmenu-example/commands/trials.js
+++
@@ -1,9 +0,0 @@
-const { SlashCommandBuilder, ContextMenuCommandBuilder, PermissionFlagsBits } = require("discord.js")
-const { readFileSync } = require("fs")
-module.exports = {
-
-    data: new ContextMenuCommandBuilder()
-    .setName("Current Trials placements")
-    .setType(2),
-   
-}
