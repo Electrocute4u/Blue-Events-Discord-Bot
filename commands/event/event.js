0b83c24eb27765722fce46048b71666278e328fc
Milim <25005864+Electrocute4u@users.noreply.github.com>
2025-04-22 00:07:56 +0200

    Added more subcommands + cleaned up code

--- commands/event/event.js
+++ commands/event/event.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require("discord.js")
+const fs = require("fs") 
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,24 +10,25 @@ module.exports = {
     .addSubcommand(subcommand =>
       subcommand
         .setName('current')
-        .setDescription('If a event is joinable/active, display info for the current event')
+        .setDescription('Displays some information for any ongoing event, if there are any.')
     )
     .addSubcommand(subcommand =>
       subcommand
         .setName('last')
-        .setDescription('Display information about the last event')
+        .setDescription('Display some information about the last event hosted.')
     )
     .addSubcommand(subcommand =>
       subcommand
         .setName('cooldown')
-        .setDescription('If a event is joinable/active, display info for the current event')
+        .setDescription('Check the event or events coooldown for a event pass holder')
         .addStringOption(option =>
           option
-          .setName("playername")
-          .setDescription("The playername of the user to check the cooldowns for")
+          .setName("player")
+          .setDescription("The name of the event pass holder to check the cooldowns for")
           .setRequired(true)
           .setMaxLength(17)
           .setMinLength(1)
+          .setAutocomplete(true)
         )
         .addStringOption(option =>
           option
@@ -35,60 +37,21 @@ module.exports = {
           .setRequired(false)
           .setMinLength(1)
           .setChoices(
-            {
-                name: "Block Party",
-                value: "woolshuffle"
-            }, {
-                name: "Floor is Lava",
-                value: "woolwars"
-            }, {
-                name: "Four Corners",
-                value: "fourcorners"
-            }, {
-                name: "Grinch Simulator",
-                value: "grinchsimulator"
-            }, {
-                name: "Mimic",
-                value: "mimic"
-            }, {
-                name: "Musical Chairs",
-                value: "musicalchairs"
-            }, {
-                name: "Red Rover",
-                value: "redrover"
-            }, {
-                name: "Showdown",
-                value: "showdown"
-            }, {
-                name: "Spleef",
-                value: "spleef"
-            }, {
-                name: "TNT Run",
-                value: "tntrun"
-            }, {
-                name: "TNT Tag",
-                value: "tnttag"
-            }, {
-                name: "Turf Wars",
-                value: "turfwars"
-            }, {
-                name: "Waterdrop",
-                value: "waterdrop"
-            }
-        )
-        )
-    )
-    .addSubcommand(subcommand =>
-      subcommand
-        .setName('stats')
-        .setDescription('Check the current event stats for a player')
-        .addStringOption(option =>
-          option
-          .setName("playername")
-          .setDescription("The playername of the user to check the stats for")
-          .setRequired(true)
-          .setMaxLength(17)
-          .setMinLength(1)
+            { name: 'Bedwars', value: 'bedwars' },
+            { name: 'Block Party', value: 'woolshuffle' },
+            { name: 'Floor is Lava', value: 'woolwars' },
+            { name: 'Four Corners', value: 'fourcorners' },
+            { name: 'Grinch Simulator', value: 'grinchsimulator' },
+            { name: 'Mimic', value: 'mimic' },
+            { name: 'Musical Chairs', value: 'musicalchairs' },
+            { name: 'Red Rover', value: 'redrover' },
+            { name: 'Showdown', value: 'showdown' },
+            { name: 'Spleef', value: 'spleef' },
+            { name: 'TNT Run', value: 'tntrun' },
+            { name: 'TNT Tag', value: 'tnttag' },
+            { name: 'Turf Wars', value: 'turfwars' },
+            { name: 'Waterdrop', value: 'waterdrop' }
+          )
         )
     )
     .addSubcommand(subcommand =>
@@ -102,108 +65,147 @@ module.exports = {
           .setRequired(true)
           .setMinLength(1)
           .setChoices(
-            {
-                name: "Block Party",
-                value: "woolshuffle"
-            }, {
-                name: "Floor is Lava",
-                value: "woolwars"
-            }, {
-                name: "Four Corners",
-                value: "fourcorners"
-            }, {
-                name: "Grinch Simulator",
-                value: "grinchsimulator"
-            }, {
-                name: "Mimic",
-                value: "mimic"
-            }, {
-                name: "Musical Chairs",
-                value: "musicalchairs"
-            }, {
-                name: "Red Rover",
-                value: "redrover"
-            }, {
-                name: "Showdown",
-                value: "showdown"
-            }, {
-                name: "Spleef",
-                value: "spleef"
-            }, {
-                name: "TNT Run",
-                value: "tntrun"
-            }, {
-                name: "TNT Tag",
-                value: "tnttag"
-            }, {
-                name: "Turf Wars",
-                value: "turfwars"
-            }, {
-                name: "Waterdrop",
-                value: "waterdrop"
-            }
+                    { name: 'Bedwars', value: 'bedwars' },
+                    { name: 'Block Party', value: 'woolshuffle' },
+                    { name: 'Floor is Lava', value: 'woolwars' },
+                    { name: 'Four Corners', value: 'fourcorners' },
+                    { name: 'Grinch Simulator', value: 'grinchsimulator' },
+                    { name: 'Mimic', value: 'mimic' },
+                    { name: 'Musical Chairs', value: 'musicalchairs' },
+                    { name: 'Red Rover', value: 'redrover' },
+                    { name: 'Showdown', value: 'showdown' },
+                    { name: 'Spleef', value: 'spleef' },
+                    { name: 'TNT Run', value: 'tntrun' },
+                    { name: 'TNT Tag', value: 'tnttag' },
+                    { name: 'Turf Wars', value: 'turfwars' },
+                    { name: 'Waterdrop', value: 'waterdrop' }
+                  )
         )
-        )
+    )
+    .addSubcommandGroup(group =>
+        group
+          .setName('leaderboard')
+          .setDescription('View leaderboard data')
+          .addSubcommand(sub =>
+            sub
+              .setName('all')
+              .setDescription('View the full leaderboard')
+              .addStringOption(option =>
+                option
+                  .setName('sort')
+                  .setDescription('Sort type for leaderboard')
+                  .addChoices(
+                    { name: 'Top', value: 'top' },
+                    { name: 'Alphabetical', value: 'alphabetical' }
+                  )
+                  .setRequired(false) // Optional, will default to 'top' in code
+              )
+           .addStringOption(option =>
+                option
+                  .setName('event')
+                  .setDescription('The event to fetch the leaderboard for')
+                  .setRequired(false)
+                  .setChoices(
+                    { name: 'Bedwars', value: 'bedwars' },
+                    { name: 'Block Party', value: 'woolshuffle' },
+                    { name: 'Floor is Lava', value: 'woolwars' },
+                    { name: 'Four Corners', value: 'fourcorners' },
+                    { name: 'Grinch Simulator', value: 'grinchsimulator' },
+                    { name: 'Mimic', value: 'mimic' },
+                    { name: 'Musical Chairs', value: 'musicalchairs' },
+                    { name: 'Red Rover', value: 'redrover' },
+                    { name: 'Showdown', value: 'showdown' },
+                    { name: 'Spleef', value: 'spleef' },
+                    { name: 'TNT Run', value: 'tntrun' },
+                    { name: 'TNT Tag', value: 'tnttag' },
+                    { name: 'Turf Wars', value: 'turfwars' },
+                    { name: 'Waterdrop', value: 'waterdrop' }
+                  )
+              )
+          )
+          .addSubcommand(sub =>
+            sub
+              .setName('player')
+              .setDescription('Check the leaderboard for a specific player')
+              .addStringOption(option =>
+                option
+                  .setName('player')
+                  .setDescription('The name of the player to check the leaderboard for')
+                  .setRequired(true)
+                  .setMinLength(1)
+                  .setMaxLength(17)
+                  .setAutocomplete(true)
+              )
+              .addStringOption(option =>
+                option
+                  .setName('event')
+                  .setDescription('The event to fetch the leaderboard for')
+                  .setRequired(false)
+                  .setChoices(
+                    { name: 'Bedwars', value: 'bedwars' },
+                    { name: 'Block Party', value: 'woolshuffle' },
+                    { name: 'Floor is Lava', value: 'woolwars' },
+                    { name: 'Four Corners', value: 'fourcorners' },
+                    { name: 'Grinch Simulator', value: 'grinchsimulator' },
+                    { name: 'Mimic', value: 'mimic' },
+                    { name: 'Musical Chairs', value: 'musicalchairs' },
+                    { name: 'Red Rover', value: 'redrover' },
+                    { name: 'Showdown', value: 'showdown' },
+                    { name: 'Spleef', value: 'spleef' },
+                    { name: 'TNT Run', value: 'tntrun' },
+                    { name: 'TNT Tag', value: 'tnttag' },
+                    { name: 'Turf Wars', value: 'turfwars' },
+                    { name: 'Waterdrop', value: 'waterdrop' }
+                  )
+              )
+          )
     )
     .addSubcommand(subcommand =>
       subcommand
-        .setName('leaderboard')
-        .setDescription('Get the overall top wins or from a specific event category')
+        .setName('stats')
+        .setDescription('Check the current event stats for a player')
         .addStringOption(option =>
           option
-          .setName("event")
-          .setDescription("The event to fetch the leaderboard for")
+          .setName("player")
+          .setDescription("The name of the player to check the stats for")
           .setRequired(true)
+          .setMaxLength(17)
           .setMinLength(1)
-          .setChoices(
-            {
-                name: "Block Party",
-                value: "woolshuffle"
-            }, {
-                name: "Floor is Lava",
-                value: "woolwars"
-            }, {
-                name: "Four Corners",
-                value: "fourcorners"
-            }, {
-                name: "Grinch Simulator",
-                value: "grinchsimulator"
-            }, {
-                name: "Mimic",
-                value: "mimic"
-            }, {
-                name: "Musical Chairs",
-                value: "musicalchairs"
-            }, {
-                name: "Red Rover",
-                value: "redrover"
-            }, {
-                name: "Showdown",
-                value: "showdown"
-            }, {
-                name: "Spleef",
-                value: "spleef"
-            }, {
-                name: "TNT Run",
-                value: "tntrun"
-            }, {
-                name: "TNT Tag",
-                value: "tnttag"
-            }, {
-                name: "Turf Wars",
-                value: "turfwars"
-            }, {
-                name: "Waterdrop",
-                value: "waterdrop"
-            }
+          .setAutocomplete(true)
         )
-    )
     )
     .addSubcommand(subcommand =>
         subcommand
           .setName('random')
           .setDescription('Unsure what event to host? Let the bot decide for you!')
-      ),
+    ),
+    async autoComplete(interaction) {
+    // Ensure the focused option is the 'player' input field
+    const focusedOption = interaction.options.getFocused(true); // get both name and value
+    if (focusedOption.name !== 'player') return;
+
+    const userInput = focusedOption.value;
+
+    // Avoid errors on empty input
+    if (!userInput || userInput.length < 1) {
+      return interaction.respond([]); // Return no suggestions
+    }
+
+    // Load leaderboard data
+    const leaderboardData = JSON.parse(
+      fs.readFileSync('./blooby_event/leaderboard.json', 'utf8')
+    );
+
+    const allPlayers = Object.keys(leaderboardData);
+
+    // Filter players based on input and limit to 25
+    const results = allPlayers
+      .filter(name => name.toLowerCase().includes(userInput.toLowerCase()))
+      .slice(0, 25)
+      .map(name => ({ name, value: name }));
+
+    await interaction.respond(results);
+    },
     async execute(interaction, bot) {
       const {readFileSync} = require("fs")
 
