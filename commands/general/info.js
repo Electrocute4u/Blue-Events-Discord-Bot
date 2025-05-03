10a3e793957fef0c5c719395cedbb6a226e3f436
Milim <25005864+Electrocute4u@users.noreply.github.com>
2025-05-03 15:19:40 +0200

    Added new commands

---
+++ commands/general/info.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require("discord.js")
+const path = require("path");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("info")
+    .setDescription("Displays system stats of the bot")
+    .setContexts(0,1,2)
+    .setIntegrationTypes(0,1),
+    async execute(interaction, tools, bot) { 
+      const commandPath = path.join(__dirname, "../../commandFunctions", path.basename(__dirname), path.basename(__filename));
+
+      delete require.cache[require.resolve(commandPath)];
+      const { command } = require(commandPath);
+      
+      await command(interaction, tools, bot);
+  }
+}
